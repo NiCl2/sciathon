a6b0a336636795f449b9b004312cc5e855b6b99e
Zaibaa <zaibaa.zp@gmail.com>
2020-06-21 09:59:09 +0100

    Front-end finished. Missing: feedback from clicking Request button. Scores pulled to extension programatically, moving on to importing info from file or db.

--- extension/javascript/content.js
+++ extension/javascript/content.js
@@ -1,4 +1,5 @@
 var domainName = "";
+var globalAvgScore = 0;
 
 var setCollapsibleEntries = function() {
 	console.log("[setCollapsibleEntries]");
@@ -30,15 +31,15 @@ var getWebsiteInformation = function(){
 	    var domain = url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
 			var pageheadings = tabs[0].title.split("-", 2);
 			var pagetitle = pageheadings[0];
-			// var pageauthor = ""; 
+			// var pageauthor = "";
 			// if (pageheadings.length > 1){
 			// 	pageauthor = pageheadings[1].concat(" :");
 			// }
 			domainName = domain;
-			
+
 			console.log("the parsed URL is: " + domain);
 			console.log("the TITLE is: " + pagetitle);
-		
+
 			document.getElementById("findTheTitle").innerHTML = pagetitle;
 			// document.getElementById("findTheAuthor").innerHTML = pageheadings;
 			document.getElementById("findTheDomain").innerHTML = domainName;
@@ -53,7 +54,7 @@ var getWebsiteTitle = function(){
 	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
 		var pagetitle = tabs[0].title;
 		console.log("the TITLE is: " + pagetitle);
-		
+
 		document.getElementById("findTheTitle").innerHTML = pagetitle;
 
 		// $("#findTheDomain").html(domainName);
@@ -62,9 +63,25 @@ var getWebsiteTitle = function(){
 });
 }
 
+var getScores = function(){
+	console.log("[Getting Sources Score]");
+	var sourceScore = 5;
+	var biasScore = 9;
+	var clarityScore = 8.8;
+	var avgScore = (sourceScore + biasScore + clarityScore) / .3;
+
+	document.getElementById("score-sources").innerHTML = sourceScore.toString().concat("/10");
+	document.getElementById("score-bias").innerHTML = biasScore.toString().concat("/10");;
+	document.getElementById("score-clarity").innerHTML = clarityScore.toString().concat("/10");;
+
+	console.log(`The average score is: ${avgScore}`);
+	globalAvgScore = avgScore;
+}
+
 var init = function(){
 	getWebsiteInformation();
 	setCollapsibleEntries();
+	getScores();
 };
 
 init();
