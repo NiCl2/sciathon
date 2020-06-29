11b602178e7713a9c1ff8cddd0eefd87ba7139e0
NiCl2 <nicholas.clifton@outlook.com>
2020-06-29 14:25:07 +0100

    remove readJSON and provide notifications through quert to mongo

--- extension/javascript/background.js
+++ extension/javascript/background.js
@@ -1,63 +1,23 @@
-/*
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-    console.log(sendResponse);    
-  });
-*/
+// matching to MONGO database 
+const API_URL = 'https://authentisci-api.herokuapp.com/api/v1/average?ad=';
 
-(function readJSON(){
-  chrome.runtime.getPackageDirectoryEntry(function(root) {
-      root.getFile("data/ratings.json", {}, function(fileEntry) {
-          fileEntry.file(function(file) {
-              var reader = new FileReader();
-              var flag = 0;
-              reader.onloadend = function(e) {
-                  var webData = JSON.parse(this.result);
-                  chrome.storage.local.set({"webData": webData});
-              };
-              reader.readAsText(file);             
-
-          });
-      });
-  });
-})();
-
-function compareUrl(key, url){
-    if (url.includes(key)) {
-        return 1;
-    } else {
-        return 0;
-    }
-  }
-
-function regCheckUrls(url, webData) {
-    console.log(url);
-    for (const key in webData.webData) {
-        comp = compareUrl(key, url);
-        if (comp === 1) {
-            return key;
+function dbCheckUrls(url) {
+  fetch(API_URL + url)
+    .then(res => res.json())
+    .then(data => {
+        if (data.score < 5) {
+            notify()
         }
-    }
-    return 0;
-    
-  };
+    }).catch(err => {
+
+    });
+
+};
  
 chrome.tabs.onActivated.addListener(function (activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function (tab) {
-        console.log(tab.url);
-        chrome.storage.local.get(['webData'], function (webData) {
-            var addressMatch = regCheckUrls(tab.url, webData);
-            if (addressMatch === 0) {
-                
-            } else {
-                var entry = webData.webData[addressMatch];
-                if (entry.score < 5) {
-                    notify()
-                }
-                
-            }
-
-        });
+        
+        dbCheckUrls(tab.url);
 
     });
 });
