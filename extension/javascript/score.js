1d86384ee113f92fb51abe95b75895f0b4a3ad19
Dominik Krzeminski <raymon92@gmail.com>
2020-09-21 20:14:48 +0100

    wroking version of submit form added

---
+++ extension/javascript/score.js
@@ -0,0 +1,50 @@
+const form = document.querySelector('#score_form');
+
+const API_ADD_URL = 'https://authentisci-api.herokuapp.com/api/v1/add';
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let formdata = new FormData(form);
+
+    const http = new XMLHttpRequest();
+    http.open('POST', API_ADD_URL, true);
+
+    http.setRequestHeader('Content-type', 'application/json');
+    http.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    var object = {};
+    formdata.forEach((value, key) => {object[key] = value});
+    var jsondata = JSON.stringify(object);
+    console.log(jsondata)
+
+    for (const kk in object) {
+        if (object[kk] == "") {
+            alert("All values need to be filled");
+            return;
+        }
+    };
+    http.onreadystatechange = function() {
+        if (http.readyState == 4 && http.status == 200) {
+            alert("Score submitted successfully!");
+        }
+        if (http.readyState == 4 && http.status != 200) {
+          alert("Problem with submitting score :( Try again later...");
+        };
+    }
+
+    http.onload = () => {
+        console.log(http.responseText);
+    };
+
+    http.send(jsondata);
+    
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    browser.tabs.query (
+         { currentWindow: true, active: true },
+         function(tabs) {
+             var activeTab = tabs[0];
+             document.getElementById("url").value = activeTab.url;
+         });
+   });
+
